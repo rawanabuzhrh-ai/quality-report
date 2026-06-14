78ca38f65d53e78ab0639bd7adfc36919158a9e2
rawanabuzhrh-ai <rawan.abuzhrh@darbalwatan.com>
2026-06-14 11:57:32 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,10 +6,11 @@
    انسخ كائن firebaseConfig من Firebase والصقه بدل القيم الفارغة.
    ============================================================ */
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCeETYOpvnNx-9yGq0fMP3p5U6EA_Ng9g",
+  authDomain: "quality-report-9380.firebaseapp.com",
+  databaseURL: "https://quality-report-9380-default-rtdb.firebaseio.com",
+  projectId: "quality-report-9380",
+  storageBucket: "quality-report-9380.firebasestorage.app",
+  messagingSenderId: "380823527115",
+  appId: "1:380823527115:web:56f5c84cbbab665c56845e"
 };
